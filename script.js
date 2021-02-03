5989311136a377e90bcdd392d13a85f3fd35cc6d
Eli <ezsto92@gmail.com>
2021-02-03 16:17:08 -0500

    beginning setup for math portion

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let cards = document.querySelectorAll(".desktop-card");
+let addExpense = document.querySelector(".add-expense");
+let weeklyBudgetSection = document.querySelector(".weekly-budget-section");
+let weeklyBudget = document.querySelector(".weekly-budget");
+let incomeInput = document.querySelector(".income-input");
+let startingBudget = 0;
+
+function updateBudget(event) {
+  event.preventDefault();
+  startingBudget = incomeInput.value;
+  weeklyBudget.innerText = "$" + startingBudget;
+  incomeInput.value = "";
+}
+console.log(updateBudget);
