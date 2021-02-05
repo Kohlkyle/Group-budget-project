70067fec50dd933fdda3f7d498553b641a04dc89
kohlkyle <kylekohl2@yahoo.com>
2021-02-05 13:52:24 -0500

    pictures

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-"use strict";
+'use strict'
 
-let weeklyForm = document.querySelector(".weekly-form");
-let weeklyBudget = document.querySelector(".weekly-budget");
-let updateBudgetButton = document.querySelector(".update-budget");
-let cards = document.querySelectorAll(".card-section");
+let weeklyForm = document.querySelector('.weekly-form')
+let weeklyBudget = document.querySelector('.weekly-budget')
+let updateBudgetButton = document.querySelector('.update-budget')
+let cards = document.querySelectorAll('.card-section')
 // ================Weekly budget ==================
 
-let weeklyIncome = 0;
+let weeklyIncome = 0
 
-weeklyForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+weeklyForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-  let snapshot = new FormData(weeklyForm);
-  weeklyIncome = snapshot.get("budget-amount");
+  let snapshot = new FormData(weeklyForm)
+  weeklyIncome = snapshot.get('budget-amount')
 
   // if (weeklyIncome > 2000) {
   //   let mask = document.createElement("div");
   //   mask.classList.add("jimCarrey");
   // }
 
-  weeklyBudget.textContent = `Weekly Budget: $${weeklyIncome}`;
-});
+  weeklyBudget.textContent = `Weekly Budget: $${weeklyIncome}`
+})
 
 // ==============Entertainment Card======================
 // let entertainment = document.querySelector(".entertainment");
 
-let nameInput = document.querySelector(".name-input");
-let entertainmentExpense = document.querySelector(".entertainment-expense");
-let entertainmentForm = document.querySelector(".entertainment-form");
+let nameInput = document.querySelector('.name-input')
+let entertainmentExpense = document.querySelector('.entertainment-expense')
+let entertainmentForm = document.querySelector('.entertainment-form')
 
-let entertainment = [];
-let entertainmentAmount = 0;
+let entertainment = []
+let entertainmentAmount = 0
 
 //===============Expense Amount $$$$$$ ==========
 
-let expenseTotal = document.querySelector(".expense-total");
-let amountLeft = document.querySelector(".amount-left");
-let addButton = document.querySelectorAll(".add-button");
+let expenseTotal = document.querySelector('.expense-total')
+let amountLeft = document.querySelector('.amount-left')
+let addButton = document.querySelectorAll('.add-button')
 
-entertainmentForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+entertainmentForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-  let snapshot = new FormData(entertainmentForm);
-  let entertainmentType = snapshot.get("entertainmentname");
+  let snapshot = new FormData(entertainmentForm)
+  let entertainmentType = snapshot.get('entertainmentname')
 
   let entertainmentObject = {
     entertainmentname: entertainmentType,
     //expense amount
-  };
+  }
 
   // ================Trying to make above function global to all cards================
   // const cardMath = (e) => {
@@ -59,31 +59,45 @@ entertainmentForm.addEventListener("submit", (e) => {
   // =================
   // ==============appends entertainment names to footer ======================
 
-  entertainmentForm.reset();
+  entertainmentForm.reset()
 
-  entertainment.push(entertainmentObject);
+  entertainment.push(entertainmentObject)
 
-  entertainmentExpense.innerHTML = "Entertainment:";
+  entertainmentExpense.innerHTML = 'Entertainment:'
 
   entertainment.forEach((item, index) => {
-    let span = document.createElement("span");
+    let span = document.createElement('span')
     if (index === 0) {
-      span.textContent = `${item.entertainmentname}`;
+      span.textContent = `${item.entertainmentname}`
     } else {
-      span.textContent = `, ${item.entertainmentname}`;
+      span.textContent = `, ${item.entertainmentname}`
     }
 
-    entertainmentExpense.append(span);
-  });
+    entertainmentExpense.append(span)
+  })
   entertainmentAmount =
-    entertainmentAmount + parseFloat(snapshot.get("amount-input"));
+    entertainmentAmount + parseFloat(snapshot.get('amount-input'))
 
-  expenseTotal.textContent = `Expense Total: $${entertainmentAmount}`;
+  expenseTotal.textContent = `Expense Total: $${entertainmentAmount}`
 
   amountLeft.textContent = `Remaining Balance: $${
     parseFloat(weeklyIncome) - parseFloat(entertainmentAmount)
-  }`;
-
+  }`
+  showImage()
   //- parseFloat(snapshot.get(expenseTotal));
   // amountLeft.textContent = `Remaining Balance: $${amountLeft}`;
-});
+})
+
+const showImage = () => {
+  let happy = document.querySelector('.happy')
+  let money = document.querySelector('.money')
+  let worried = document.querySelector('.worried')
+  let badGuy = document.querySelector('.bad-guy')
+  if (amountLeft >= 2000) {
+    money.classList.remove('money')
+  } else if (amountLeft < 2000 && amountLeft > 700) {
+    happy.classList.remove('happy')
+  } else {
+    worried.classList.remove('worried')
+  }
+}
